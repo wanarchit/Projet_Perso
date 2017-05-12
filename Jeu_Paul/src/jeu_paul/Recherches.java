485605eee54487a7683d62d307c2a286f2145dbf
Paul <Paul@192.168.1.95>
2017-05-12 22:13:40 +0200

    recherches

--- Jeu_Paul/src/jeu_paul/Recherches.java
+++ Jeu_Paul/src/jeu_paul/Recherches.java
@@ -1,1 +1,80 @@
-
+//Utilisateurs : Peut posséder plusieurs personnages;
+//- Nom ?
+//- Prénom ?
+//- Age ?
+//- Liste de personnages
+//- Login
+//- Mot de passe
+//
+//Personnages :
+//- Pseudo
+//- Classe ?  -> Modifie : pouvoir spécial, type (résistance/faiblesse), skin, caractéristique de base, équipement possible
+//- Race ?
+//- Choix perso (à la place de classe/race)  -> classe à part
+//- Niveau -> Commence au niveau 1 et niveau max = ?
+//- Expérience -> Augmente avec le niveau
+//- Argent
+//- Sexe : fille/garçon ? intérêt (changer le skin/caract ?)
+//- Points de vie (endurance ?)
+//- Mana / Karma / Magie / Rage / Concentration .. ?
+//- Etat de santé -> Altération dEtat possible (poison, paralysie, gel, feu ...)
+//- Caractéristiques (classe à part)
+//- Inventaire (classe à part)
+//- Poids inventaire (caractéristiques)
+//- Compétence : augmenter avec des objets spéciaux (!= argent) (cristaux ?)
+//- Equipement : tete, corps, jambe, pied, main, bijoux, cape
+//            + Armes avec différents types dAttaque (4 comme pokémon ?)
+//                - Types : magiques, distantes (arc), cac (épé,hache ...), bouclier, livre de sort
+//                        2 mains, 1 main
+//
+//
+//Caractéristiques :
+//- Endurance ? (Point de vie ? / Poids de linventaire)
+//- Force (cac)
+//- Defense (cac)
+//- Attaque_spéciale (magie/dist)
+//- Defence_spéciale (magie/dist)
+//- Critique ? -> x2 dégat, 1 chance sur Score_Crit dapparaitre;
+//- Vitesse ?
+//
+//
+//Enemie :
+//- Nom
+//- Classe ? ou Type énnemie -> affecte les faiblesses/résistances
+//- Niveau
+//- Caractéristiques (cf classe caract) / utiliser le critique ?
+//- Point de vie ? 
+//- Equipements (porté)
+//- Inventaire ?
+//- Loot : argent + objet + armes/équipement porté ? (bof ...)
+//
+//
+//PNJ : 
+//- Donneur de quêtes
+//- Vendeur
+//- PNJ inutile (juste pour parler)
+//- PNJ objectif de quête
+//
+//Objets :
+//- Clé
+//- Argent ? (inclure dans objets ou pas ? -> Bof)
+//- Cristaux ? -> idem mais bof
+//
+//Lieux:
+//- salle de coffre (stockage inventaire)
+//- Soin (cuisine, chambre avec lit ?) / orbes de vie / mana ... ? / Objets de soins ?
+//- Salle dentrainement
+//- Systeme de "salle/donjons" qui se reset quand on sort/meurt  --> retour dans un centre/ville/entrée ... ?
+//        => Accès débloqué à un certain niveau
+//
+//
+//
+//Systemes autres :
+//- Rareté des objets ? => loot aléatoire selon rareté/difficulté
+//- Récompense ?
+//- Systeme de score de réussite dun donjon ?
+//- Tutoriel
+//- Map
+//- Inventaire
+//- Jouer au clavier ! => déplacement du personnage
+//
